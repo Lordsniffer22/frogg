efbf68bd776760ac05b615c56184d0ec0a74dfb8
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-07-16 20:10:05 +0300

    Update servers.py

--- servers.py
+++ servers.py
@@ -1,7 +1,1 @@
-# Server credentials
-SERVER_CREDENTIALS = {
-    'USA': {'host': '159.100.22.216',
-           'username': 'root',
-           'password': '4L389IRtB2YU83J2@@8J1P'
-           }
-}
+
