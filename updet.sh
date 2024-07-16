545bdd521c2584e7f431e4d7285bf4dd6b2bc9d5
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-07-16 20:14:35 +0300

    Update updet.sh

--- updet.sh
+++ updet.sh
@@ -9,7 +9,6 @@ docker exec -it frog rm -rf frog.py
 wget -O keys.py https://raw.githubusercontent.com/Lordsniffer22/frogg/main/keys.py
 wget -O frog.py https://raw.githubusercontent.com/Lordsniffer22/frogg/main/frog.py
 docker cp frog.py frog:/app
-docker cp keys.py frog:/app
 rm frog.py
 rm keys.py
 docker restart frog
